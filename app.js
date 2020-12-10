7335fbcc2923bac7621cd091668dc2748f0ef26e
ztm1762530@163.com <ztm1762530@163.com>
2020-12-10 20:56:13 +0800

    feat:

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const express = require("express");
 const app = express();
 const colors = require("colors-console");
 const bodyParse = require("body-parser");
-
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    " Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", " 3.2.1");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 let port = 9999;
 const userRoute = require("./routes/UserRoutes");
 const loginRoute = require("./routes/LoginRoutes");
