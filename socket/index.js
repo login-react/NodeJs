45abe5000a304d5ef0b6cc16faf0b9d5ae11bea2
ztm1762530@163.com <ztm1762530@163.com>
2021-01-02 14:00:09 +0800

    feat:

--- socket/index.js
+++ socket/index.js
@@ -2,7 +2,6 @@ module.exports = function (io) {
   io.on("connection", (socket) => {
     console.log("🙎‍♀️ socket.io start success");
     socket.on("chatsEmit", (val) => {
-      console.log("val :>> ", val);
       // 如果 server端 接收到 则insert
       socket.broadcast.emit("reveives", val);
     });
